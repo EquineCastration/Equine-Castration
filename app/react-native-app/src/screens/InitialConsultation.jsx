3ef677d9df3ce7e886b1fea1b95cd468ddd839ee
jivanam <shasondev@gmail.com>
2023-03-24 14:18:45 +0000

    Improve step button and fix position to bottom

--- app/react-native-app/src/screens/InitialConsultation.jsx
+++ app/react-native-app/src/screens/InitialConsultation.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, View } from "react-native";
+import { SafeAreaView, View, Button } from "react-native";
 import { Formik } from "formik";
 
 import { CheckBox } from "../components/CheckBox";
@@ -49,13 +49,13 @@ const handleInitialConsultationSubmit = (navigation, data) => {
 const Layout = ({ children, backNavigation }) => {
   return (
     <SafeAreaView className="flex-1">
-      <ScrollView className="mt-8 px-5">
+      <View className="flex-1 mt-8 px-5">
         <InitialConsultationHeader
           className="mt-10"
           backNavigation={backNavigation}
         />
         {children}
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -90,31 +90,35 @@ export const InitialConsultationStepOne = ({ navigation }) => {
         }}
       >
         {({ handleChange, handleSubmit, values, setFieldValue }) => (
-          <View className="my-2">
-            <InputField
-              label="Horse name:"
-              onChangeText={handleChange("horseName")}
-              value={values?.horseName}
-            />
-            <InputField
-              label="Client surname:"
-              onChangeText={handleChange("clientSurname")}
-              value={values?.clientSurname}
-            />
-            <DatePickerField
-              name="dateOfCastration"
-              label="Date of castration:"
-              type="date"
-              value={values?.dateOfCastration}
-              setFieldValue={setFieldValue}
-            />
-            <BasicTouchableOpacity
-              title="Next"
-              onPress={() => {
-                handleSubmit();
-                navigation.navigate("InitialConsultationStepTwo");
-              }}
-            />
+          <View className="flex-1">
+            <View className="my-2 flex-1 justify-center">
+              <InputField
+                label="Horse name:"
+                onChangeText={handleChange("horseName")}
+                value={values?.horseName}
+              />
+              <InputField
+                label="Client surname:"
+                onChangeText={handleChange("clientSurname")}
+                value={values?.clientSurname}
+              />
+              <DatePickerField
+                name="dateOfCastration"
+                label="Date of castration:"
+                type="date"
+                value={values?.dateOfCastration}
+                setFieldValue={setFieldValue}
+              />
+            </View>
+            <View className="justify-end mb-12">
+              <Button
+                onPress={() => {
+                  handleSubmit();
+                  navigation.navigate("InitialConsultationStepTwo");
+                }}
+                title="Next"
+              />
+            </View>
           </View>
         )}
       </Formik>
@@ -139,15 +143,19 @@ export const InitialConsultationStepTwo = ({ navigation }) => {
         }}
       >
         {({ handleSubmit, values, setFieldValue }) => (
-          <View className="my-2">
-            <CheckBox
-              label="Is horse less than 2 years old ?"
-              value={values?.isLessThanTwo}
-              onValueChange={(isLessThanTwo) =>
-                setFieldValue("isLessThanTwo", isLessThanTwo)
-              }
-            />
-            <BasicTouchableOpacity title="Submit" onPress={handleSubmit} />
+          <View className="flex-1">
+            <View className="my-2 flex-1 justify-center">
+              <CheckBox
+                label="Is horse less than 2 years old ?"
+                value={values?.isLessThanTwo}
+                onValueChange={(isLessThanTwo) =>
+                  setFieldValue("isLessThanTwo", isLessThanTwo)
+                }
+              />
+            </View>
+            <View className="justify-end mb-12">
+              <Button onPress={handleSubmit} title="submit" />
+            </View>
           </View>
         )}
       </Formik>
