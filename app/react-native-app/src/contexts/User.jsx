9a5a50ea7c39eea3319d7f91d50299eb28e344c7
Shason Gurung <s.gurung@nottingham.ac.uk>
2023-04-21 11:56:20 +0100

    Resolve pnpm lock file issue and also added checks for effective auto login

--- app/react-native-app/src/contexts/User.jsx
+++ app/react-native-app/src/contexts/User.jsx
@@ -27,8 +27,40 @@ export const UserProvider = ({ children }) => {
   const { data: profile, mutate } = useProfile();
 
   useEffect(() => {
-    // clearAsyncStorage();
-    setUser(profile);
+    // this is where our cookies suppose to be stored
+    const checkAsyncStorage = async () => {
+      // list of cookies item in async storage to look for
+      const cookiesExpected = [
+        ".EquineCastration.Config",
+        ".EquineCastration.Profile",
+        ".EquineCastration.Identity",
+      ];
+      try {
+        // we are checking if cookies exist
+        // as cookies would normally be cleared when user logs out
+        // and available if logged in or has been authenticated before
+        const isAllCookiesFound = await Promise.all(
+          cookiesExpected.map(async (item) => {
+            const itemValue = await AsyncStorage.getItem(item);
+            return itemValue !== null && itemValue !== undefined;
+          })
+        ).then((results) => results.every(Boolean));
+
+        // if above is true,
+        // this only we will set user with profile
+        if (isAllCookiesFound) {
+          // TODO: Look into how client was able to make authenticated request even without cookies (not Backend issue)
+          // could have simply set user with profile
+          // as only authenticated user can make api calls to get their profile
+          // but, for some reason on Android, valid cookies are being added from somewhere even after clearing cookies
+          // thus, this check was added to restrict that
+          setUser(profile);
+        }
+      } catch (error) {
+        console.log("Error reading cookies from Async Storage", error);
+      }
+    };
+    checkAsyncStorage();
   }, [profile]);
 
   const signOut = () => setUser(null);
