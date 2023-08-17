6045940738ef59e79a0881b3fc3c899a2d4fefb4
Jivanam <shasondev@gmail.com>
2023-08-17 19:21:29 +0100

    Update CaseSummary component.

--- app/react-native-app/src/components/CaseSummary.jsx
+++ app/react-native-app/src/components/CaseSummary.jsx
@@ -5,6 +5,15 @@ import { colors } from "style/style";
 
 export const CaseSummary = ({ data }) => {
   const fields = initialConsultation.fields;
+
+  const formatValue = (field) => {
+    let value = data[field].toString();
+
+    Array.isArray(data[field]) && (value = `${data[field].join(", ")}`); // if array, join with comma
+    typeof data[field] === "boolean" && (value = data[field] ? "Yes" : "No"); // if boolean, return Yes or No
+    return value;
+  };
+
   return (
     <View
       style={{
@@ -13,36 +22,47 @@ export const CaseSummary = ({ data }) => {
         paddingHorizontal: 15,
         borderRadius: 10,
         backgroundColor: colors.ui.bg,
+        width: "100%",
       }}
     >
-      {Object.keys(data).map((item, index) => (
-        <View
-          key={index}
-          style={{
-            marginVertical: 2,
-            paddingVertical: 10,
-            flexDirection: "row",
-            borderBottomWidth: 1,
-            borderColor: colors.primary[100],
-          }}
-        >
-          <Text
-            style={{
-              fontWeight: 300,
-              fontSize: font.size.sm,
-            }}
-          >{`${fields[item]?.label} -`}</Text>
-          <Text
-            style={{
-              fontWeight: 500,
-            }}
-          >
-            {Array.isArray(data[item])
-              ? `${data[item].join(", ")}`
-              : data[item]}
-          </Text>
-        </View>
-      ))}
+      {Object.keys(data).map(
+        (item, index) =>
+          data[item].toString() && // if not empty string &&
+          data[item] !== 0 && ( // if not 0
+            <View
+              key={index}
+              style={{
+                marginVertical: 2,
+                paddingVertical: 10,
+                flexDirection: "row",
+                borderBottomWidth: 1,
+                borderColor: colors.primary[100],
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: 300,
+                  fontSize: font.size.sm,
+                  maxWidth: "65%",
+                }}
+              >
+                {fields[item]?.label}
+              </Text>
+              <Text>-</Text>
+              <Text
+                style={{
+                  fontWeight: 500,
+                  flexWrap: "wrap",
+                  flex: 1,
+                }}
+              >
+                {formatValue(item)}
+              </Text>
+            </View>
+          )
+      )}
     </View>
   );
 };
