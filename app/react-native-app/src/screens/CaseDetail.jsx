de5645ec58b53fda56343b9ed12738ae772f06c4
Shason Gurung <s.gurung@nottingham.ac.uk>
2023-08-18 17:12:26 +0100

    Display toast when deleting case.

--- app/react-native-app/src/screens/CaseDetail.jsx
+++ app/react-native-app/src/screens/CaseDetail.jsx
@@ -6,21 +6,47 @@ import { useUser } from "contexts/User";
 import { permissions } from "constants/site-permissions";
 import { useBackendApi } from "contexts/BackendApi";
 import { ActionButton } from "components/ActionButton";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import { useState, useEffect } from "react";
+import { Spinner } from "components/Spinner";
 
 export const CaseDetail = ({ navigation, route }) => {
   const { caseData } = route.params;
+  const [feedback, setFeedback] = useState();
+  const [isDeleting, setIsDeleting] = useState(false);
   const { user } = useUser();
   const {
     case: { delete: remove },
   } = useBackendApi();
 
+  useEffect(() => {
+    feedback &&
+      Toast.show({
+        type: feedback.status,
+        text1: feedback.message,
+      });
+  }, [feedback]);
+
   const handleDelete = async (caseId) => {
+    setIsDeleting(true);
     try {
       await remove(caseId);
+      setFeedback({
+        status: "success",
+        message: `Case id -${caseId} removed successfully!`,
+      });
       navigation.goBack();
     } catch (e) {
-      console.log(e);
+      const error = await e.response;
+      switch (error?.status) {
+        default:
+          setFeedback({
+            status: "error",
+            message: "An unknown error has occurred.",
+          });
+      }
     }
+    setIsDeleting(false);
   };
 
   const canDelete = user.permissions.some((permission) =>
@@ -47,46 +73,49 @@ export const CaseDetail = ({ navigation, route }) => {
   );
 
   return (
-    <DefaultLayout>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          margin: 5,
-          gap: 5,
-        }}
-      >
-        {canEdit && (
-          <ButtonAction
-            title="Edit"
-            iconName="create-outline"
-            color={colors.patra[900]}
-            onPress={() =>
-              navigation.navigate("EditInitialConsultation", {
-                editData: caseData,
-              })
-            }
-          />
-        )}
+    <>
+      {isDeleting ? <Spinner text="Deleting case" /> : null}
+      <DefaultLayout>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            margin: 5,
+            gap: 5,
+          }}
+        >
+          {canEdit && (
+            <ButtonAction
+              title="Edit"
+              iconName="create-outline"
+              color={colors.patra[900]}
+              onPress={() =>
+                navigation.navigate("EditInitialConsultation", {
+                  editData: caseData,
+                })
+              }
+            />
+          )}
 
-        {canDelete && (
-          <ButtonAction
-            title="Delete"
-            iconName="trash-outline"
-            color={colors.error}
-            onPress={() => handleDelete(caseData.id)}
-          />
-        )}
-      </View>
-      <ScrollView
-        style={{
-          marginVertical: 2,
-          marginHorizontal: 5,
-          paddingHorizontal: 5,
-        }}
-      >
-        <CaseSummary data={caseData} />
-      </ScrollView>
-    </DefaultLayout>
+          {canDelete && (
+            <ButtonAction
+              title="Delete"
+              iconName="trash-outline"
+              color={colors.error}
+              onPress={() => handleDelete(caseData.id)}
+            />
+          )}
+        </View>
+        <ScrollView
+          style={{
+            marginVertical: 2,
+            marginHorizontal: 5,
+            paddingHorizontal: 5,
+          }}
+        >
+          <CaseSummary data={caseData} />
+        </ScrollView>
+      </DefaultLayout>
+    </>
   );
 };
