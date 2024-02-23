366b567ffc03a8ed06a153c6b8952bef9233211e
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-02-23 09:54:54 +0000

    DischargeEmailService for sending discharge email including email view, one for straight discharge and another for discharge with registration request.

---
+++ app/EquineCastration/Config/AppDownloadOptions.cs
@@ -0,0 +1,7 @@
+namespace EquineCastration.Config;
+
+public class AppDownloadOptions
+{
+  public string AppStoreLink { get; set; } = string.Empty;
+  public string GooglePlayStoreLink { get; set; } = string.Empty;
+}
---
+++ app/EquineCastration/Services/EmailServices/DischargeEmailService.cs
@@ -0,0 +1,46 @@
+using EquineCastration.Config;
+using EquineCastration.Models.Emails;
+using EquineCastration.Services.Contracts;
+using Microsoft.Extensions.Options;
+
+namespace EquineCastration.Services.EmailServices;
+
+public class DischargeEmailService
+{
+  private readonly IEmailSender _emails;
+  private readonly AppDownloadOptions _download;
+
+  public DischargeEmailService(IEmailSender emails, IOptions<AppDownloadOptions> downloadOptions)
+  {
+    _emails = emails;
+    _download = downloadOptions.Value;
+  }
+  
+  public async Task SendDischargeWithRegistrationRequest(EmailAddress to, string horseName, string dischargeDate,
+    string veterinarianName)
+    => await _emails.SendEmail(
+      to,
+      "Emails/DischargeRegistrationRequest",
+      new DischargeEmailModel(
+        to.Name!,
+        horseName,
+        dischargeDate,
+        veterinarianName,
+        _download.AppStoreLink,
+        _download.GooglePlayStoreLink
+      ));
+  
+  public async Task SendDischarge(EmailAddress to, string horseName, string dischargeDate,
+    string veterinarianName)
+    => await _emails.SendEmail(
+      to,
+      "Emails/Discharge",
+      new DischargeEmailModel(
+        to.Name!,
+        horseName,
+        dischargeDate,
+        veterinarianName,
+        _download.AppStoreLink,
+        _download.GooglePlayStoreLink
+      ));
+}
