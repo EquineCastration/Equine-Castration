9973cbd978ca749221e795c8e6e316dd918685d5
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-03-01 00:53:41 +0000

    Undo Horse entity update

--- app/EquineCastration/Data/Entities/Horse.cs
+++ app/EquineCastration/Data/Entities/Horse.cs
@@ -6,17 +6,5 @@ public class Horse
 {
   public int Id { get; set; }
   public string Name { get; set; } = string.Empty;
-  public string Breed { get; set; } = string.Empty;
-  public string? BreedOther { get; set; }
-  public int Weight { get; set; }
-  public int Age { get; set; }
-  public string OwnershipLength { get; set; } = string.Empty;
-  public bool IsClinicallyHealthy { get; set; }
-  public string IsClinicallyHealthyNo { get; set; } = string.Empty;
-  public bool IsOnMedication { get; set; }
-  public string IsOnMedicationYes { get; set; } = string.Empty;
-  public string LocationTesticleLeft { get; set; } = string.Empty;
-  public string LocationTesticleRight { get; set; } = string.Empty;
-  public DateTimeOffset DateOfCastration { get; set; }
   public Owner Owner { get; set; } = null!;
 }
