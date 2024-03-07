c847207edbb641bd65726ae56cd1d2c654824c82
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-03-07 02:31:11 +0000

    Screen component. Useful when creating new screen as this already has the boilerplate for setting up screen.

---
+++ app/react-native-app/src/components/Screen.jsx
@@ -0,0 +1,20 @@
+import { useStyle } from "contexts/StyleProvider";
+import { SafeAreaView } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+export const Screen = ({ children, props }) => {
+  const { colors } = useStyle();
+  return (
+    <SafeAreaView
+      style={[container, { backgroundColor: colors.background, ...props }]}
+    >
+      <KeyboardAwareScrollView>{children}</KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+};
+
+const container = {
+  flex: 1,
+  height: "100%",
+  width: "100%",
+};
