e2790827b7371bc18b6b03c26245ef39bb4a1e34
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-05-08 14:28:12 +0100

    Add new property

--- app/Data/Entities/Identity/Owner.cs
+++ app/Data/Entities/Identity/Owner.cs
@@ -6,4 +6,5 @@ public class Owner
   public string Email { get; set; } = string.Empty;
   public string? ApplicationUserId { get; set; }
   public ApplicationUser ApplicationUser { get; set; } = null!;
+  public bool OptOut { get; set; }
 }
