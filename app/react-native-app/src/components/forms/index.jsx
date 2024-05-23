73e3fe4518e1ae40ea4bd0d05f34cafdbfdca2be
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-05-23 22:29:35 +0100

    Move email and passwordfield component into the forms folder

--- app/react-native-app/src/components/forms/index.jsx
+++ app/react-native-app/src/components/forms/index.jsx
@@ -4,4 +4,6 @@ export * from "./InputField";
 export * from "./GroupCheckBoxField";
 export * from "./ToggleField";
 export * from "./SliderField";
+export { EmailField } from "./EmailField";
+export { PasswordField } from "./PasswordField";
 export { DatePickerField } from "./DatePickerField";
