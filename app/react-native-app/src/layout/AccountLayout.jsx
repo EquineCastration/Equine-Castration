6c55fd5da479f4e9048f1d25087edaeb70cbd55a
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-05-23 22:33:20 +0100

    Add logo to AccountLayout and improve UI

--- app/react-native-app/src/layout/AccountLayout.jsx
+++ app/react-native-app/src/layout/AccountLayout.jsx
@@ -1,8 +1,13 @@
-import { View, SafeAreaView, Text } from "react-native";
-import { BgGradient } from "components/BgGradient";
-import { colors, font } from "style/style";
-import { BasicBackButton } from "components/BasicBackButton";
+import { View, SafeAreaView, Image } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "components/Button";
+import { Ionicons } from "@expo/vector-icons";
+import { useStyle } from "contexts/StyleProvider";
+import { Text } from "components/Text";
+import { spacing } from "style";
+
+const LOGO_URL = "../../assets/eq_logo.jpg";
 
 export const AccountLayout = ({
   children,
@@ -11,53 +16,72 @@ export const AccountLayout = ({
   secondaryHeading = "Headin 2",
   backBtn,
 }) => {
+  const { colors: colorScheme } = useStyle();
+  const navigation = useNavigation();
   return (
     <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
+        backgroundColor: colorScheme?.background,
       }}
     >
       <View
         style={{
           height: headerViewHeight,
-          justifyContent: "center",
+          justifyContent: "flex-end",
         }}
       >
-        <BgGradient>
-          {backBtn && (
-            <View style={{ position: "absolute", top: 50, left: 20 }}>
-              <BasicBackButton color={colors.light} />
-            </View>
-          )}
-          <View style={{ alignItems: "center", marginBottom: 40, gap: 5 }}>
-            <Text
-              style={{
-                fontSize: font.size["6xl"],
-                fontWeight: 500,
-                color: colors.light,
-              }}
-            >
-              {primaryHeading}
-            </Text>
-            <Text
-              style={{
-                fontSize: font.size["lg"],
-                color: colors.light,
-              }}
-            >
-              {secondaryHeading}
-            </Text>
+        {backBtn && (
+          <View style={{ position: "absolute", top: 50, left: 20 }}>
+            <Button
+              preset="ghost"
+              onPress={() => navigation.goBack()}
+              LeftAccessory={() => (
+                <Ionicons
+                  name="arrow-back-outline"
+                  size={20}
+                  color={colorScheme?.text}
+                />
+              )}
+            />
           </View>
-        </BgGradient>
+        )}
+        <View
+          style={{
+            alignItems: "center",
+            gap: spacing.xx,
+          }}
+        >
+          <Image
+            source={require(LOGO_URL)}
+            style={{ width: 100, height: 100, marginBottom: spacing.sm }}
+          />
+
+          <Text
+            size="xxl"
+            weight="bold"
+            style={{
+              color: colorScheme?.textLink,
+            }}
+          >
+            {primaryHeading}
+          </Text>
+          <Text
+            size="lg"
+            weight="medium"
+            style={{
+              color: colorScheme?.textLink,
+            }}
+          >
+            {secondaryHeading}
+          </Text>
+        </View>
       </View>
       <View
         style={{
           flex: 1,
-          backgroundColor: colors.light,
-          marginTop: -20,
-          padding: 10,
-          borderRadius: 20,
+          padding: spacing.md,
         }}
       >
         <KeyboardAwareScrollView>{children}</KeyboardAwareScrollView>
