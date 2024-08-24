69d10dd2a34e8956f80bbf2a8396efd6ae992984
Shason Gurung <s.gurung@nottingham.ac.uk>
2024-08-24 15:40:54 +0100

    Add docs.

---
+++ docs/theme.config.jsx
@@ -0,0 +1,18 @@
+export default {
+  logo: (
+    <>
+      <img
+        src="/eq_logo.jpg"
+        alt="Equine Castration Logo"
+        style={{ width: "50px" }}
+      />
+      <span style={{ marginLeft: ".4em", fontWeight: 700 }}>
+        Equine Castration Documentation
+      </span>
+    </>
+  ),
+  project: {
+    link: "https://github.com/EquineCastration/Equine-Castration",
+  },
+  primaryHue: 189,
+};
